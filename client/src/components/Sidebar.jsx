3f030c763516f17d4f46453e12e843743f2d2f1d
Yaman Adamnor <yamix2000@gmail.com>
2020-04-23 23:55:09 +0200

    Groundwork for Sidebar component

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -13,13 +13,48 @@ const Sidebar = () => {
     fetchRoutes();
   }, []);
 
-  console.log(routes);
 
   return (
     <div className="sidebar">
-      hej
+      <ul>
+        <SidebarItem type="title">
+          <h3>
+            Movies
+          </h3>
+        </SidebarItem>
+        <li className="sidebar-item sidebar-sublist">
+          <ul>
+            <li> GET Movies</li>
+            <li> POST Movies</li>
+            <li> PATCH Movies</li>
+            <li> DELETE Movies</li>
+          </ul>
+        </li>
+        <li>
+
+
+          <ul className="sidebar-item sidebar-sublist">
+            <li> GET Shows</li>
+            <li> POST Shows</li>
+            <li> PATCH Shows</li>
+            <li> DELETE Shows</li>
+          </ul>
+        </li>
+      </ul>
     </div>
   );
 };
 
-export default Sidebar;
+const SidebarItem = (props) => {
+  // console.log(children);
+
+  return (
+    <li className= {` sidebar-item ${ props.type == "title" ? "sidebar-title" : ""` }>
+      {props.children}
+    </li>
+  );
+};
+export {
+  Sidebar,
+  SidebarItem,
+};
